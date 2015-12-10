43ab8ee70d586c4bebaf9edd90344a3b610f16c9
matteogabburo <matteogabburo@gmail.com>
2015-12-10 01:07:11 +0100

    client fixed

--- src/client/Assignment3Client.java
+++ src/client/Assignment3Client.java
@@ -28,32 +28,43 @@ public class Assignment3Client
 
     public static void main(String[] args) throws Exception {
 
-        PeopleService service = new PeopleService();
-        People people = service.getPeopleImplementationPort();
+        /*PeopleService service = new PeopleService();
+        People people = service.getPeopleImplementationPort();*/
 
+        String serverUrl = "https://guarded-anchorage-1835.herokuapp.com/ws/people?wsdl";
 
-        /*URL url = new URL("http://127.0.1.1:6902");
+        URL url = new URL(serverUrl);
         //1st argument service URI, refer to wsdl document above
         //2nd argument is service name, refer to wsdl document above
         QName qname = new QName("http://ws.document.introsde/", "PeopleService");
         Service service = Service.create(url, qname);
 
         People people = service.getPort(People.class);
-*/
+
         Assignment3Client a = new Assignment3Client();
+
+        String tmp = "";
 
         //READ PERSON LIST
 
-        a.log += "\n\n#1 READ PERSON LIST ==============\n";
+        tmp += serverUrl+"\n\n";
+
+        tmp += "\n\n#1 READ PERSON LIST ==============\n";
         List<Person> pList = people.readPersonList();
         for(int i = 0; i < pList.size(); i++)
-            a.printPerson(pList.get(i));
+            tmp += a.printPerson(pList.get(i));
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
-        a.log += "\n\n#2 READ PERSON ==============\n";
-        Person p1 = people.readPerson((long) 151);
-        a.printPerson(p1);
+        tmp += "\n\n#2 READ PERSON ==============\n";
+        Person p1 = people.readPerson((long) 1);
+        tmp +=a.printPerson(p1);
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
-        a.log += "\n\n#3 UPDATE PERSON ==============\n";
+        tmp += "\n\n#3 UPDATE PERSON ==============\n";
         Person p3 = new Person();
         p3.setLastname(p1.getLastname());
         p3.setId(p1.getId());
@@ -64,68 +75,98 @@ public class Assignment3Client
             p3.setFirstname("Ambrogio");
 
         p1 = people.updatePerson(p3);
-        a.printPerson(p1);
+        tmp += a.printPerson(p1);
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
-        a.log += "\n\n#4 CREATE PERSON ==============\n";
+        tmp += "\n\n#4 CREATE PERSON ==============\n";
         Person p2 = a.makePerson("Artemisio", "Rossi");
         p2 =people.createPerson(p2);
-        a.printPerson(p2);
+        tmp = a.printPerson(p2);
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
-        a.log += "\n\n#5 DELETE PERSON ==============\n";
+        tmp += "\n\n#5 DELETE PERSON ==============\n";
         people.deletePerson(p2.getId());
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
+        tmp += "\n\nDelete..\nCHECK IF CREATED PERSON IS REALLY DELETED\n";
+        pList = people.readPersonList();
+        for(int i = 0; i < pList.size(); i++)
+            tmp += a.printPerson(pList.get(i));
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
         //READ PERSON HISTORY
-        a.log += "\n\n#6 READ PERSON HISTORY ==============\n";
+        tmp += "\n\n#6 READ PERSON HISTORY ==============\n";
         List<Measure> mList = people.readPersonHistory(p1.getId(), "height");
-        System.out.println(p1.getId() +"  "+ mList.size());
         Measure m;
         for(int i = 0; i < mList.size(); i++)
         {
             m = mList.get(i);
-            a.printMeasure(m);
+            tmp += a.printMeasure(m);
         }
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
         //READ MEASURE TYPES
-        a.log += "\n\n#7 READ MEASURE TYPES ==============\n";
+        tmp += "\n\n#7 READ MEASURE TYPES ==============\n";
         List<String> types = people.readMeasureTypes();
         System.out.println("Types");
         for(int i = 0; i < types.size(); i++)
         {
-            a.log += types.get(i);
+            tmp += types.get(i)+"\n";
         }
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
         //READ PERSON MEASURES
-        a.log += "\n\n#8 READ PERSON MEASURE ==============\n";
+        tmp += "\n\n#8 READ PERSON MEASURE ==============\n";
         mList = people.readPersonMeasures(p1.getId(), "height" ,(long)1);
         for(int i = 0; i < mList.size(); i++)
         {
             m = mList.get(i);
-            a.printMeasure(m);
+            tmp += a.printMeasure(m);
         }
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
 
         //SAVE PERSON MEASURE
-        a.log += "\n\n#9 SAVE PERSON MEASURE ==============\n";
+        tmp += "\n\n#9 SAVE PERSON MEASURE ==============\n";
         Measure m1 = a.makeMeasure(1, "height", "123", "integer", p1);
         people.savePersonMeasure(p1.getId(), m1);
         mList = people.readPersonMeasures(p1.getId(), "height" ,(long)1);
         for(int i = 0; i < mList.size(); i++)
         {
             m = mList.get(i);
-            a.printMeasure(m);
+            tmp += a.printMeasure(m);
         }
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
         //UPDATE PERSON MEASURE
-        a.log += "\n\n#10 UPDATE PERSON MEASURE ==============\n";
+        tmp += "\n\n#10 UPDATE PERSON MEASURE ==============\n";
         m1.setMeasureValue("678");
         m1 = people.updatePersonMeasure(p1.getId(), m1);
         mList = people.readPersonMeasures(p1.getId(), "height" ,(long)1);
         for(int i = 0; i < mList.size(); i++)
         {
             m = mList.get(i);
-            a.printMeasure(m);
+            tmp += a.printMeasure(m);
         }
+        a.log += tmp;
+        System.out.println(tmp);
+        tmp = "";
 
         a.makeLogs(a.log);
     }
@@ -157,40 +198,48 @@ public class Assignment3Client
         return p;
     }
 
-    public void printPerson(Person p)
+    public String printPerson(Person p)
     {
-        this.log += "PERSON =========================================";
-        this.log +="\n";
-        this.log +="id : \t"+p.getId();
-        this.log +="firstName : \t"+p.getFirstname()+"\n";
-        this.log +="lastName : \t"+p.getLastname()+"\n";
+        String res = "";
+
+        res += "PERSON =========================================";
+        res +="\n";
+        res +="id : \t"+p.getId()+"\n";
+        res +="firstName : \t"+p.getFirstname()+"\n";
+        res +="lastName : \t"+p.getLastname()+"\n";
 
         Measure m;
 
-        this.log +="HealthHistory"+"\n";
+        res +="HealthHistory"+"\n";
         List<Measure> pList = p.getHealthHistory();
         for(int i = 0; i < pList.size(); i++)
         {
             m = pList.get(i);
-            this.printMeasure(m);
+            res += this.printMeasure(m);
         }
-        this.log +="CurrentHealth"+"\n";
+        res +="CurrentHealth"+"\n";
         pList = p.getCurrentHealth();
         for(int i = 0; i < pList.size(); i++)
         {
             m = pList.get(i);
-            this.printMeasure(m);
+            res += this.printMeasure(m);
         }
-        this.log +="\n";
-        this.log +="================================================\n";
+        res +="\n";
+        res +="================================================\n";
+
+        return res;
     }
 
-    public void printMeasure(Measure m)
+    public String printMeasure(Measure m)
     {
-        this.log +="\tmid :\t"+m.getMid()+"\n";
-        this.log +="\tdateRegistered :\t"+m.getDateRegistered()+"\n";
-        this.log +="\tmeasureType :\t"+m.getMeasureType()+"\n";
-        this.log +="\tmeasureValue :\t"+m.getMeasureValue()+"\n";
+        String res = "";
+
+        res +="\tmid :\t"+m.getMid()+"\n";
+        res +="\tdateRegistered :\t"+m.getDateRegistered()+"\n";
+        res +="\tmeasureType :\t"+m.getMeasureType()+"\n";
+        res +="\tmeasureValue :\t"+m.getMeasureValue()+"\n";
+
+        return res;
     }
 
     private void makeLogs(String xmlResp) throws IOException {
